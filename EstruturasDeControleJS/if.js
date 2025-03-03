0c9a5fe63f6c87f9d695cbf5ddb01fcb6bc694bb
wesmartins96 <wesmartins96@gmail.com>
2025-03-03 01:52:07 -0300

    estruturas de controle

---
+++ EstruturasDeControleJS/if.js
@@ -0,0 +1,21 @@
+
+
+
+function promocaoEscolar(nota) {
+    if (nota >= 7) {
+        console.log('Parabéns, passou de ano!');
+
+    }
+}
+promocaoEscolar(8);
+
+
+function teste1(num) {
+    if (num > 7)
+        console.log(num);
+    console.log('Final');
+}
+
+teste1(6);
+teste1(8);
+
---
+++ EstruturasDeControleJS/ifElse.js
@@ -0,0 +1,12 @@
+
+
+const imprimirResult = function (nota) {
+    if (nota >= 7) {
+        console.log('Aprovado!');
+    } else {
+        console.log('Reprovado!');
+    }
+}
+
+imprimirResult(5);
+imprimirResult(9);
